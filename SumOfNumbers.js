7fa0050b24987bea52a48c8ded7f493daa2254cb
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-04-28 17:07:36 +0530

    [Ganesh] Add:Sum of three numbers is Zero code is added

---
+++ SumOfNumbers.js
@@ -0,0 +1,8 @@
+function SumOfNumbers()
+{
+    let firstNumber=10
+    let secondNumber=15
+    let thirdNumber=-(firstNumber+secondNumber);
+    console.log(firstNumber+secondNumber+thirdNumber);
+}
+SumOfNumbers()
